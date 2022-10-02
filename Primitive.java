dbd5acab9b80eb5b747b47b4c2dc245399d92155
sagar <sagarbehera156@gmail.com>
2022-10-02 20:07:34 +0530

    Primitive Values

---
+++ Primitive.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class Primitive {
+static int a;
+static char b;
+static byte c;
+static boolean d;
+static double e;
+static float f;
+static long g;
+static short h;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Int default value:"+a);
+		System.out.println("Char default value:"+b);
+		System.out.println("Byte default value:"+c);
+		System.out.println("Boolean default value:"+d);
+		System.out.println("Docble default value:"+e);
+		System.out.println("Float default value:"+f);
+		System.out.println("Long default value:"+g);
+		System.out.println("Short default value:"+h);
+	}
+
+}
