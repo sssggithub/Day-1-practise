cc0bcfb32d9f1479af4925a6c0a8d549bbd0ec8d
sagar <sagarbehera156@gmail.com>
2022-10-02 20:04:04 +0530

    Equal String

---
+++ Equal_str.java
@@ -0,0 +1,20 @@
+package day1;
+import java.util.*;
+public class Equal_str {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner r=new Scanner(System.in);
+System.out.println("Enter first String");
+String str1=r.nextLine();
+System.out.println("Enter secind String");
+String str2=r.nextLine();
+if(str1==str2) {
+	System.out.println("String is equal");
+}
+else {
+	System.out.println("String is equal");
+}
+	}
+
+}
